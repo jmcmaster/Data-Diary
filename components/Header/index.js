d27a3b012d7f355d97d456a6425345768845e89f
Jason McMaster <jmcmaster@pushhere.com>
2017-12-22 10:13:43 -0500

    Created directory structure for scenes and components. Added initial components to create a header and initial view.

---
+++ components/Header/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+class Header extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Diary</Text> 
+      </View>
+    );
+  }
+}
+
+export default Header;
